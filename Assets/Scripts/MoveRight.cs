4427b6f916f67de0ebfe061784d5291f49224a1f
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-05-24 11:34:12 +0300

    Create non-ecs example

---
+++ Assets/Scripts/MoveRight.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MoveRight : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.right * (speed * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private int spawnCount;
+    [SerializeField] private float randomRadius;
+    [SerializeField] private GameObject spawnedPrefab;
+
+    private void Start()
+    {
+        for (var i = 0; i < spawnCount; i++)
+        {
+            var randomPoint = Random.insideUnitCircle * randomRadius;
+            var t = transform;
+            var spawnedInstance = Instantiate(spawnedPrefab,
+                t.position + new Vector3(randomPoint.x, randomPoint.y), Quaternion.identity, t);
+            spawnedInstance.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+        }
+    }
+}
